a3e9ae4e4eba5ae1e3a4716ada841bcda6aa8277
alexander-azizi-martin <alexander.azizi.martin@gmail.com>
2022-01-19 00:50:45 -0500

    style(client): :speech_balloon: Update website title

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -3,7 +3,14 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Inventory Manager</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
