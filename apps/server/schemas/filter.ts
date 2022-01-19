f06e8472db5876319225ba00be7a47badf8fbd88
alexander-azizi-martin <alexander.azizi.martin@gmail.com>
2022-01-18 22:42:27 -0500

    fix(server): :pencil2: Fix typo

--- apps/server/schemas/filter.ts
+++ apps/server/schemas/filter.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const filterSchema = Joi.object({
   title: Joi.string().max(50),
-  status: Joi.string().valid('ACTIVE', 'DRAFT', 'ARCHIVED'),
+  status: Joi.string().valid('ACTIVE', 'DRAFT', 'ARCHIVE'),
   productType: Joi.string().max(50),
   vendor: Joi.string().max(50),
   tag: Joi.string().max(50),
